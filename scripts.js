ff17353b56039c5b15109d1b6fb3396bb8f9be0d
Kelly Cook <kellycook20878@gmail.com>
2019-03-05 10:19:39 -0600

    added notes to the random number counter

--- scripts.js
+++ scripts.js
@@ -4,8 +4,13 @@ function randomNumber(upper) {
 
 var counter = 0;
 
-while ( counter < 10000 ) {
+while ( counter < 10 ) {
+    // generates a random number from 1-6
     var randNum = randomNumber(6);
+    // writing to the dom and adds a space afterwards
     document.write(randNum + ' ');
+    // adds 1 to the counter variable
     counter += 1;
+    // checks the while condition and checks to see if the number is less than 10
+    // the program will end once the counter is greater than 10
 }
